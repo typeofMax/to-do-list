31303da9a8ebca9b4d7863db2e9d79a5dcca0bb6
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-02-22 01:57:04 +0300

    fixed: line is too long

--- js/main.js
+++ js/main.js
@@ -265,7 +265,9 @@ const tasks = [
             'afterbegin',
             `
             <div class="alert alert-primary d-flex align-items-center" role="alert">
-                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg>
+                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:">
+                    <use xlink:href="#info-fill"/>
+                </svg>
                 <div>
                     Не выполненные задачи отсутствуют
                 </div>
